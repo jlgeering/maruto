1eee3e2ec4f88f318ea0075b0d0e85ee5ccca53c
Jean-Luc Geering <jlgeering.13@gmail.com>
2013-04-01 17:43:06 +0200

    use minites-reporters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,6 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+# https://github.com/CapnKernul/minitest-reporters
+MiniTest::Reporters.use!
+# MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
