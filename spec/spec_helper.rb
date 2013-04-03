71f9c0992281a16ef91d33c2b412bda01a8c4fab
Jean-Luc Geering <jlgeering.13@gmail.com>
2013-04-03 19:54:01 +0200

    disable MiniTest::Reporters, not compatible with 1.8.7 ?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
-require 'minitest/reporters'
 
 # https://github.com/CapnKernul/minitest-reporters
-MiniTest::Reporters.use!
+# require 'minitest/reporters'
+# MiniTest::Reporters.use!
 # MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 
