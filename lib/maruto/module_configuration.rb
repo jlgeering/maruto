970253783238d3ec8e1ae8178c7d6c27c37d3b3e
Jean-Luc Geering <jlgeering.13@gmail.com>
2013-04-06 10:40:09 +0200

    spec for module version

---
+++ lib/maruto/module_configuration.rb
@@ -0,0 +1,33 @@
+require 'maruto/base'
+require 'nokogiri'
+
+module Maruto::ModuleConfiguration
+
+	def self.read_module_version(m, xml_node)
+
+		if xml_node.nil?
+			m[:warnings] ||= []
+			m[:warnings] << "module:#{m[:name]} - config.xml is missing a <modules></modules> node (in '#{m[:config_path]}')"
+			return m
+		end
+
+		unless xml_node.at_xpath("./#{m[:name]}")
+			m[:warnings] ||= []
+			m[:warnings] << "module:#{m[:name]} - config.xml is missing a <modules><#{m[:name]}></#{m[:name]}></modules> node (in '#{m[:config_path]}')"
+		end
+
+		xml_node.xpath("./*").each do |n|
+			unless n.name.to_sym == m[:name]
+				m[:warnings] ||= []
+				m[:warnings] << "module:#{m[:name]} - config.xml contains configuration for a different module (<modules><#{n.name}></#{n.name}></modules> in '#{m[:config_path]}')"
+			end
+		end
+
+		if xml_node.at_xpath("./#{m[:name]}/version")
+			m[:version] = xml_node.at_xpath("./#{m[:name]}/version").content
+		end
+
+		m
+	end
+
+end
