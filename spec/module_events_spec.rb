5c453acfd88457ccbe2410038ecb04fd512ad007
Jean-Luc Geering <jlgeering.13@gmail.com>
2013-04-11 20:39:42 +0200

    split spec in multiple files

--- spec/module_events_spec.rb
+++ spec/module_events_spec.rb
@@ -6,65 +6,7 @@ module Maruto
 
 	describe ModuleConfiguration do
 
-		before do
-			@module_a = { :name => :Mage_A, :active => true, :code_pool => :core, :defined => 'a', :config_path => 'app/code/core/Mage/A/etc/config.xml' }
-			@module_b = { :name => :Mage_B, :active => true, :code_pool => :core, :defined => 'b', :config_path => 'app/code/core/Mage/B/etc/config.xml' }
-			@module_c = { :name => :Mage_C, :active => true, :code_pool => :core, :defined => 'c', :config_path => 'app/code/core/Mage/C/etc/config.xml' }
-			@module_d = { :name => :Mage_D, :active => true, :code_pool => :core, :defined => 'd', :config_path => 'app/code/core/Mage/D/etc/config.xml' }
-		end
-
 		describe "when parsing a module config.xml" do
-			describe "and reading the module version" do
-				before do
-					@xml_module_version = Nokogiri::XML('''
-						<modules>
-							<Mage_A>
-								<version>0.0.1</version>
-							</Mage_A>
-						</modules>
-					''').root.at_xpath('/modules')
-				end
-				it "will return the module definition hash" do
-					m = ModuleConfiguration.read_module_version(@module_a, @xml_module_version)
-					m.must_be_kind_of Hash
-					m.must_equal @module_a
-				end
-				it "will add the version to the hash" do
-					m = ModuleConfiguration.read_module_version(@module_a, @xml_module_version)
-					m.must_include :version
-					m[:version].must_equal '0.0.1'
-				end
-				it "will add a warning when then modules node is missing" do
-					w = @module_a.merge({ :warnings => ['first warning'] })
-					m = ModuleConfiguration.read_module_version(w, nil)
-					m[:warnings].size.must_equal 2
-					m[:warnings][-1].must_include '<modules'
-				end
-				it "will add a warning when then node with the module's name is missing" do
-					w = @module_a.merge({ :warnings => ['first warning'] })
-					m = ModuleConfiguration.read_module_version(w, Nokogiri::XML('<modules></modules>').root.at_xpath('/modules'))
-					m[:warnings].size.must_equal 2
-					m[:warnings][-1].must_include '<modules'
-					m[:warnings][-1].must_include '<Mage_A'
-				end
-				it "will add a warning when there is a node from a different module" do
-					@xml_node = Nokogiri::XML('''
-						<modules>
-							<Mage_A>
-								<version>0.0.1</version>
-							</Mage_A>
-							<Mage_B>
-								<version>0.0.1</version>
-							</Mage_B>
-						</modules>
-					''').root.at_xpath('/modules')
-					w = @module_a.merge({ :warnings => ['first warning'] })
-					m = ModuleConfiguration.read_module_version(w, @xml_node)
-					m[:warnings].size.must_equal 2
-					m[:warnings][-1].must_include 'Mage_B'
-				end
-			end
-
 			describe "and reading events observers" do
 				before do
 					@xml_node = Nokogiri::XML('''
@@ -138,6 +80,34 @@ module Maruto
 					e[1][:name].must_equal 'second_event'
 					e[1][:observers].size.must_equal 2
 				end
+
+				# TODO multiple global xml nodes?
+
+				# it "will warn when an event has no observers" do
+				# 	@xml_node_no_obs = Nokogiri::XML('''
+				# 		<events><first_event></first_event></events>
+				# 	''').root.at_xpath('/events')
+				# 	e = ModuleConfiguration.parse_events_observers(@xml_node_no_obs)
+				# 	e.size.must_equal 1
+				# 	e[0].must_include :warnings
+				# 	e[0][:warnings].size.must_equal 1
+				# end
+				# it "will warn when an event has an empty observers node" do
+				# 	@xml_node_empty_obs = Nokogiri::XML('''
+				# 		<events><first_event><observers></observers></first_event></events>
+				# 	''').root.at_xpath('/events')
+				# 	e = ModuleConfiguration.parse_events_observers(@xml_node_empty_obs)
+				# 	e.size.must_equal 1
+				# 	e[0][:warnings].size.must_equal 1
+				# end
+				# it "will warn if an observers type is invalid" do
+				# 	e = ModuleConfiguration.parse_events_observers(@xml_node)
+				# 	e[1][:warnings].size.must_equal 1
+				# 	e[1][:warnings][0].must_include '<type>something</type>'
+				# end
+				# it "will warn if an observers class is invalid" do
+				# 	# TODO
+				# end
 			end
 
 			it "will group events by scope and build the correct path" do
---
+++ spec/module_version_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+require 'maruto/module_configuration'
+
+module Maruto
+
+	describe ModuleConfiguration do
+
+		before do
+			@module_a = { :name => :Mage_A, :active => true, :code_pool => :core, :defined => 'a', :config_path => 'app/code/core/Mage/A/etc/config.xml' }
+			@module_b = { :name => :Mage_B, :active => true, :code_pool => :core, :defined => 'b', :config_path => 'app/code/core/Mage/B/etc/config.xml' }
+			@module_c = { :name => :Mage_C, :active => true, :code_pool => :core, :defined => 'c', :config_path => 'app/code/core/Mage/C/etc/config.xml' }
+			@module_d = { :name => :Mage_D, :active => true, :code_pool => :core, :defined => 'd', :config_path => 'app/code/core/Mage/D/etc/config.xml' }
+		end
+
+		describe "when parsing a module config.xml" do
+			describe "and reading the module version" do
+				before do
+					@xml_module_version = Nokogiri::XML('''
+						<modules>
+							<Mage_A>
+								<version>0.0.1</version>
+							</Mage_A>
+						</modules>
+					''').root.at_xpath('/modules')
+				end
+				it "will return the module definition hash" do
+					m = ModuleConfiguration.read_module_version(@module_a, @xml_module_version)
+					m.must_be_kind_of Hash
+					m.must_equal @module_a
+				end
+				it "will add the version to the hash" do
+					m = ModuleConfiguration.read_module_version(@module_a, @xml_module_version)
+					m.must_include :version
+					m[:version].must_equal '0.0.1'
+				end
+				it "will add a warning when then modules node is missing" do
+					w = @module_a.merge({ :warnings => ['first warning'] })
+					m = ModuleConfiguration.read_module_version(w, nil)
+					m[:warnings].size.must_equal 2
+					m[:warnings][-1].must_include '<modules'
+				end
+				it "will add a warning when then node with the module's name is missing" do
+					w = @module_a.merge({ :warnings => ['first warning'] })
+					m = ModuleConfiguration.read_module_version(w, Nokogiri::XML('<modules></modules>').root.at_xpath('/modules'))
+					m[:warnings].size.must_equal 2
+					m[:warnings][-1].must_include '<modules'
+					m[:warnings][-1].must_include '<Mage_A'
+				end
+				it "will add a warning when there is a node from a different module" do
+					@xml_node = Nokogiri::XML('''
+						<modules>
+							<Mage_A>
+								<version>0.0.1</version>
+							</Mage_A>
+							<Mage_B>
+								<version>0.0.1</version>
+							</Mage_B>
+						</modules>
+					''').root.at_xpath('/modules')
+					w = @module_a.merge({ :warnings => ['first warning'] })
+					m = ModuleConfiguration.read_module_version(w, @xml_node)
+					m[:warnings].size.must_equal 2
+					m[:warnings][-1].must_include 'Mage_B'
+				end
+			end
+		end
+	end
+end
