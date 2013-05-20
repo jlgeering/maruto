1faab041acd5b84b40978be75bdb8e4f91db1025
Jean-Luc Geering <jlgeering.13@gmail.com>
2013-05-20 15:20:04 +0200

    magento version spec (for older magento versions)

--- lib/maruto/magento_version.rb
+++ lib/maruto/magento_version.rb
@@ -6,14 +6,44 @@ module Maruto::MagentoVersion
 	def self.read_magento_version()
 		mage = 'app/Mage.php'
 		File.open mage do |file|
-			if file.find { |line| line =~ /getVersionInfo/ }
+			# newer magento version have a getVersionInfo function
+			newer = file.find { |line| line =~ /getVersionInfo/ }
+			file.rewind
+			if newer
 				# newer Magento version
 				return []
 			else
 				# older Magento version
-				 return [1,0]
+				function = read_function(file, 'getVersion')
+				match    = function.match(/return '(\d)\.(\d)\.?(\d)?\.?(\d)?';/)
+				version    = []
+				version[0] = match[1].to_i unless match[1].nil?
+				version[1] = match[2].to_i unless match[2].nil?
+				version[2] = match[3].to_i unless match[3].nil?
+				version[3] = match[4].to_i unless match[4].nil?
+				return version
 			end
 		end
+	end
+
+private
+
+	def self.read_function(file, function_name)
+		func = nil
+		file.rewind
+		file.each_line do |line|
+			if func then
+				func += line
+				if /}/ =~ line
+					break
+				end
+			else
+				if /public static function #{function_name}\(\)/ =~ line
+					func = line
+				end
+			end
+		end
+		func.delete("\n")
 	end
 
 end
--- spec/magento_version_spec.rb
+++ spec/magento_version_spec.rb
@@ -7,7 +7,8 @@ module Maruto
 	describe "when reading the magento version" do
 
 		before do
-			@magento_root_1_0 = File.expand_path('../../fixtures/magento_1.0', __FILE__)
+			@magento_root_1_0     = File.expand_path('../../fixtures/magento_1.0', __FILE__)
+			@magento_root_1_3_3_0 = File.expand_path('../../fixtures/magento_1.3.3.0', __FILE__)
 		end
 
 		it "will return an array" do
@@ -19,9 +20,13 @@ module Maruto
 		it "will read version 1.0" do
 			Dir.chdir(@magento_root_1_0) do
 				version = MagentoVersion.read_magento_version()
-				version.size.must_equal 2
-				version[0].must_equal 1
-				version[1].must_equal 0
+				version.must_equal [1,0]
+			end
+		end
+		it "will read version 1.3.3.0" do
+			Dir.chdir(@magento_root_1_3_3_0) do
+				version = MagentoVersion.read_magento_version()
+				version.must_equal [1,3,3,0]
 			end
 		end
 
